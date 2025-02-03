887e51cb9257662e9e0677cdbd427f8f043dc900
FlorianNeveu <florian.neveu78@hotmail.fr>
2025-02-03 18:55:14 +0100

    Starting scss

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import axiosInstance from "../axiosConfig";
+import { useNavigate } from "react-router-dom";
+import "../styles/Home.scss";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [randomProducts, setRandomProducts] = useState([]);
   const API_URL = process.env.API_URL || 'https://atelier-jesmarite-production.up.railway.app';
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await axiosInstance.get("/products");
         setProducts(response.data);
+
+        // Choisir 2 produits aléatoires pour mobile et 4 pour desktop
+        const selectedProducts = response.data.sort(() => Math.random() - 0.5); // Mélange les produits
+        setRandomProducts(selectedProducts.slice(0, window.innerWidth <= 768 ? 2 : 4)); // 2 produits sur mobile, 4 sur desktop
       } catch (error) {
         console.error("Erreur lors de la récupération des produits :", error);
       }
@@ -20,7 +28,7 @@ const Home = () => {
 
   const handleAddToCart = async (productId) => {
     const sessionId = localStorage.getItem("sessionId");
-    
+
     try {
       await axiosInstance.post("/carts", {
         product_id: productId,
@@ -33,17 +41,21 @@ const Home = () => {
     }
   };
 
+  const handleProductClick = (productId) => {
+    navigate(`/products/${productId}`); // Redirige vers la page produit
+  };
+
   return (
     <div className="home">
       <h1>Bienvenue sur l'Atelier de Jesmarite</h1>
+      <h2>Découvrez notre sélection</h2>
       <div className="product-list">
-        {products.map((product) => (
-          <div key={product.id} className="product-card">
+        {randomProducts.map((product) => (
+          <div key={product.id} className="product-card" onClick={() => handleProductClick(product.id)}>
             <h2>{product.name}</h2>
             <img src={`${API_URL}${product.image}`} alt={product.name} />
             <p>{product.description}</p>
             <p>{product.price} €</p>
-            <button onClick={() => handleAddToCart(product.id)}>Ajouter au panier</button>
           </div>
         ))}
       </div>
