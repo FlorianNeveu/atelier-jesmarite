635badd41705acbc0994c8911e779ba8eb60c473
FlorianNeveu <florian.neveu78@hotmail.fr>
2025-02-04 16:45:48 +0100

    update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,7 +53,7 @@ const login = async (req, res) => {
     
     res.cookie('token', token, {
       httpOnly: false,
-      secure: false, 
+      secure: 'production', 
       sameSite: 'None',
       domain: '.vercel.app',
       maxAge: 86400000,
